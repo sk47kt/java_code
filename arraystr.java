d095d68cf9415f3398c15ff51f43f920c2084a18
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-05-19 20:32:39 +0900

    배열 값 하나씩 출력하기 ( 반복문 )
    배열에 들어있는 값 들을 반복문으로 배열의 끝까지 하나의 데이터씩 출력한다

---
+++ arraystr.java
@@ -0,0 +1,15 @@
+package uploadcode;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		char[] text = {'¾È','³ç','ÇÏ','¼¼','¿ä'};
+		for (int x=0;x<text.length;x++)
+		{
+			System.out.println(text[x]);
+		}
+	}
+
+}
