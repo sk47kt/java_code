f6d1ee1255b1afcb92f62184ea07e0eb2538cc1f
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-05-19 20:33:42 +0900

    Update arraystr.java

--- arraystr.java
+++ arraystr.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class array {
 
 	public static void main(String[] args) {
-		char[] text = {'안','녕','하','세',''};
+		char[] text = {'안','녕','하','세','요'};
 		for (int x=0;x<text.length;x++)
 		{
 			System.out.println(text[x]);
