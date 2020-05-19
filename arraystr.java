1224c33c2f6212864eac4aa4c849ce805c1df989
sk47kt <65600966+sk47kt@users.noreply.github.com>
2020-05-19 20:35:58 +0900

    배열 값 하나씩 출력하기 ( 반복문 )
    배열 값 하나씩 출력하기
    배열로 지정되어있는 값 '안' '녕 '하' '세' '요' 총 5개의 0~4 인덱스의 값을 하나씩 출력함
    결과는
    안
    녕
    하
    세
    요

---
+++ arraystr.java
@@ -0,0 +1,15 @@
+﻿package uploadcode;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		char[] text = {'안','녕','하','세','요'};
+		for (int x=0;x<text.length;x++)
+		{
+			System.out.println(text[x]);
+		}
+	}
+
+}
